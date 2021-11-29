2cf6cd4632aa0d9fcdf2f5f6371e48883f0d84dd
Greg Testo <gregt@pursuit.org>
2021-11-29 12:00:20 -0500

    added lifecycles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import './App.css';
 class App extends Component{
   constructor(){
     super();
-    this.state={};
+    this.state={
+      count: 0
+    };
+    console.log("Trigger: Constructor");
   }
+
+  componentDidMount(){
+    console.log("Trigger: On Mount");
+  }
+
+  componentDidUpdate(){
+    console.log("Trigger: On Update");
+  }
+
+  componentWillUnmount(){
+    console.log("Trigger: On Unmount");
+  }
+
+  handleIncrement=()=>{
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+
   render(){
+    console.log("Trigger: Render");
+
     return(
       <div>
         <h1>Hello World</h1>
-        <button>Click me</button>
+        <div>{this.state.count}</div>
+        <input onChange={this.handleInputChange} />
       </div>
     )
   }
